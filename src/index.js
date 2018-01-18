70c6f01f39a7781c191d28c48a5a15d2753c014d
BakerAO <dpshunter@gmail.com>
2018-01-18 16:06:05 -0600

    Finished search bar with lodash debounce delay
    Signed-off-by: BakerAO <dpshunter@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -18,17 +19,23 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term: 'cats'}, (videos) => {
+        this.videoSearch('dogs');
+    }
+    
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
             });
         });
     }
+    
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
