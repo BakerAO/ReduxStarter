d582d37b8bf2e1a113e77d43ebd5c12ddfddcc23
Rexomnis <dpshunter@gmail.com>
2018-01-18 11:46:47 -0600

    Adding API key and SearchBar functionality
    Signed-off-by: Rexomnis <dpshunter@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyD71W-Ig0hmg3ieMNeGPaDQwmaNbAZ9slE';
+
 // Create a new component (view)
-
-
-
-
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+};
 
 // Take the component's HTML and put it in the Document Object Model
-
+ReactDOM.render(<App />, document.querySelector('.container'));
